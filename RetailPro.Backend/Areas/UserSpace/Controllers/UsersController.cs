67a0c42f29a0c4c28e81c3141357c134e8c30fa5
InDieTasten <indietasten@gmail.com>
2018-10-24 21:20:14 +0200

    Add odata endpoints and swashbuckle

---
+++ RetailPro.Backend/Areas/UserSpace/Controllers/UsersController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RetailPro.Backend.Areas.UserSpace.DbContexts;
+using RetailPro.Backend.Areas.UserSpace.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace RetailPro.Backend.Areas.UserSpace.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = false)]
+    [Route("odata/[controller]")]
+    public class UsersController : ODataController
+    {
+        private readonly UserDbContext _context;
+
+        public UsersController(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [EnableQuery]
+        public async Task<IActionResult> Get() => Ok(await _context.Users.AsNoTracking().ToListAsync());
+
+        //[HttpGet]
+        //[EnableQuery]
+        //public async Task<IActionResult> Get(Guid key)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    User user = await _context.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Id == key);
+
+        //    return user == null ? NotFound() : (IActionResult)Ok(user);
+        //}
+
+        [HttpPut]
+        [EnableQuery]
+        public async Task<IActionResult> Put(Guid key, User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (key != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await UserExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        [EnableQuery]
+        public async Task<IActionResult> Post(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+        [HttpDelete]
+        [EnableQuery]
+        public async Task<IActionResult> Delete(Guid key)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            User user = await _context.Users.FindAsync(key);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        private async Task<bool> UserExists(Guid id) => await _context.Users.AsNoTracking().AnyAsync(e => e.Id == id);
+    }
+}
--- RetailPro.Backend/Startup.cs
+++ RetailPro.Backend/Startup.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.OData.Edm;
+using RetailPro.Backend.Areas.UserSpace.Models;
+using RetailPro.Backend.Areas.UserSpace.DbContexts;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.AspNetCore.Mvc.Internal;
+using System.Linq;
+using Microsoft.AspNet.OData.Formatter;
+using Microsoft.Net.Http.Headers;
 
 namespace RetailPro.Backend
 {
@@ -25,7 +29,32 @@ namespace RetailPro.Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<PlaceholderDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("PlaceholderDbContext")));
+            services.AddDbContext<UserDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("UserDbContext")));
+            services.AddDbContext<ContentDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ContentDbContext")));
+
+            services.AddOData();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("beta", new Info { Title = "RetailPro.Backend", Version = "beta" });
+            });
+
+            services.AddMvc(options =>
+            {
+                // Workaround: https://github.com/OData/WebApi/issues/1177
+                foreach (var outputFormatter in options.OutputFormatters.OfType<ODataOutputFormatter>().Where(_ => _.SupportedMediaTypes.Count == 0))
+                {
+                    outputFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/prs.odatatestxx-odata"));
+                }
+                foreach (var inputFormatter in options.InputFormatters.OfType<ODataInputFormatter>().Where(_ => _.SupportedMediaTypes.Count == 0))
+                {
+                    inputFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/prs.odatatestxx-odata"));
+                }
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,13 +70,28 @@ namespace RetailPro.Backend
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc(routes =>
+
+            app.UseSwagger();
+            app.UseSwaggerUI(o => o.SwaggerEndpoint("/swagger/beta/swagger.json", "RetailPro.Backend"));
+
+            app.UseMvc(routeBuilder =>
             {
-                routes.MapRoute(
-                  name: "areas",
-                  template: "{area:exists}/{controller=Me}/{action=Get}/{id?}"
-                );
+                // Workaround: https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/581
+                routeBuilder.DefaultHandler = app.ApplicationServices.GetRequiredService<MvcRouteHandler>();
+
+                routeBuilder.Select().Expand().Filter().OrderBy().MaxTop(200).Count();
+                routeBuilder.MapODataServiceRoute("odata", "odata", GetEdmModel());
+
+                // Workaround: https://github.com/OData/WebApi/issues/1175
+                routeBuilder.EnableDependencyInjection();
             });
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<User>("Users");
+            return builder.GetEdmModel();
         }
     }
 }
